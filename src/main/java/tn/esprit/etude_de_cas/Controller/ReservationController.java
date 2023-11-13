9494fd6b223fb0f9841d251d788dbfa035c9a841
sarra <sarra.touhami@esprit.tn>
2023-11-13 22:47:51 +0100

    ReservationController and UniversiteController

--- src/main/java/tn/esprit/etude_de_cas/Controller/ReservationController.java
+++ src/main/java/tn/esprit/etude_de_cas/Controller/ReservationController.java
@@ -1,9 +1,28 @@
 package tn.esprit.etude_de_cas.Controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.etude_de_cas.Entity.Reservation;
+import tn.esprit.etude_de_cas.Service.IReservation;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class ReservationController {
+    private IReservation iReservation;
+    @PostMapping("/retrieveAllReservation")
+    public List<Reservation> retrieveAllReservation() {
+        return iReservation.retrieveAllReservation();
+    }
+
+    @PutMapping("/updateReservation")
+    public Reservation updateReservation(Reservation reservation) {
+        return iReservation.updateReservation(reservation);
+    }
+
+   @GetMapping("/retrieveReservation/{idReservation}")
+    public Reservation retrieveReservation(@PathVariable String idReservation) {
+        return iReservation.retrieveReservation(idReservation);
+    }
 }
--- src/main/java/tn/esprit/etude_de_cas/Controller/UniversiteController.java
+++ src/main/java/tn/esprit/etude_de_cas/Controller/UniversiteController.java
@@ -1,9 +1,33 @@
 package tn.esprit.etude_de_cas.Controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.etude_de_cas.Entity.University;
+import tn.esprit.etude_de_cas.Service.IUniversite;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class UniversiteController {
+    private IUniversite iUniversite;
+    @GetMapping("/retrieveAllUniversities")
+    public List<University> retrieveAllUniversities() {
+        return iUniversite.retrieveAllUniversities();
+    }
+
+    @PostMapping("/addUniversite")
+    public University addUniversite(University university) {
+        return iUniversite.addUniversite(university);
+    }
+
+    @PutMapping("/updateUniversite")
+    public University updateUniversite(University university) {
+        return iUniversite.updateUniversite(university);
+    }
+
+    @GetMapping("/retrieveUniversite/{idUniversite}")
+    public University retrieveUniversite(@PathVariable long idUniversite) {
+        return iUniversite.retrieveUniversite(idUniversite);
+    }
 }
