04e7bfcf11c7fab29c63df22c859a03ef553b40a
Mohsen <mohsen.bernaoui@gmail.com>
2023-11-13 23:31:47 +0100

    @Repository

--- src/main/java/tn/esprit/etude_de_cas/Reposity/ChambreRepo.java
+++ src/main/java/tn/esprit/etude_de_cas/Reposity/ChambreRepo.java
@@ -1,7 +1,8 @@
 package tn.esprit.etude_de_cas.Reposity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.etude_de_cas.Entity.Chambre;
-
+@Repository
 public interface ChambreRepo extends JpaRepository<Chambre,Long> {
 }
--- src/main/java/tn/esprit/etude_de_cas/Reposity/ReservationRepo.java
+++ src/main/java/tn/esprit/etude_de_cas/Reposity/ReservationRepo.java
@@ -1,7 +1,8 @@
 package tn.esprit.etude_de_cas.Reposity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.etude_de_cas.Entity.Reservation;
-
+@Repository
 public interface ReservationRepo extends JpaRepository<Reservation,String> {
 }
