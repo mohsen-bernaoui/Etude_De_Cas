9756e306b348b7e77b62d6860e44d2309aede9a2
Mohsen <mohsen.bernaoui@gmail.com>
2023-12-03 19:11:45 +0100

    Cross origin

--- src/main/java/tn/esprit/etude_de_cas/Controller/ReservationController.java
+++ src/main/java/tn/esprit/etude_de_cas/Controller/ReservationController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 public class ReservationController {
     private IReservation iReservation;
