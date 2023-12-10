368640cf775ce9a909b71a3b5640d9d2a7debf98
BahaEddinDridi <bahaeddine170@gmail.com>
2023-12-10 19:36:13 +0100

    cleaning

--- src/main/java/tn/esprit/etude_de_cas/Service/IUniversite.java
+++ src/main/java/tn/esprit/etude_de_cas/Service/IUniversite.java
@@ -6,12 +6,7 @@ import java.util.List;
 
 public interface IUniversite {
     List<University> retrieveAllUniversities();
-
     University addUniversite (University university);
-
     University updateUniversite (University university);
-
     University retrieveUniversite (long idUniversite);
-
-
 }
